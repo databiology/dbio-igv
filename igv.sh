9dae0fc433a4af3ac3773b6c8afcf2e89ae255ec
Felipe Leza <felipe.leza@databiology.com>
2017-11-22 10:51:02 -0600

    Changed configurations in igv.sh file and adding fullscreen in rc.xml

---
+++ igv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#This script is intended for launch on *nix machines
+
+#Script must be in the same directory as igv.jar
+#Add the flag -Ddevelopment = true to use features still in development
+MEMORY="-Xmx$(free -m | grep 'Mem' | awk '{print $4}')m"
+PREFIX=$(dirname "$(readlink "$0" || echo "$0")")
+exec java "$MEMORY" \
+-XX:+IgnoreUnrecognizedVMOptions \
+    --illegal-access=permit --add-modules=java.xml.bind \
+	-Dapple.laf.useScreenMenuBar=true \
+	-Djava.net.preferIPv4Stack=true \
+	-jar "$PREFIX"/igv.jar "$@"
+
+    
