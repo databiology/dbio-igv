7a5f20c672bdc56c817498224569a8ec58b3a736
Hugo Ontiveros <hugo.ontiveros@databiology.com>
2020-03-23 11:38:34 -0600

    Fix check for output files, test

--- no_output.sh
+++ no_output.sh
@@ -3,10 +3,10 @@
 # Carlos De Blas <carlos.deblas@databiology.com>
 # (C) 2019 Databiology Inc.
 
-if find "/scratch/restults" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
+if find "/scratch/results" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
     echo "Results directory not empty."
 else
-    echo -e "IGV log file\nNo output was generated\nAuto generated file" >> /scratch/results/IGV_log.txt
+    echo -e "IGV log file\nApp finished\nAuto generated file" >> /scratch/results/IGV_log.txt
 fi
 
 touch /scratch/done
