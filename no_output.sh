9f0509470d201bc3d5760ceb9a79800225d2a1e8
Hugo Ontiveros <hugo.ontiveros@databiology.com>
2020-03-23 15:07:13 -0600

    Fix no_output.sh

--- no_output.sh
+++ no_output.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Entrypoint for Beast application
 # Carlos De Blas <carlos.deblas@databiology.com>
-# (C) 2019 Databiology Inc.
+# (C) 2020 Databiology Inc.
 
 if find "/scratch/results" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
     echo "Results directory not empty."
