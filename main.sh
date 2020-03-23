e1f8c6cedef6a8e51dd21cf7eb4fd8c0d5d35cf6
Hugo Ontiveros <hugo.ontiveros@databiology.com>
2020-03-23 09:28:50 -0600

    Fix check for output files, test

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Entrypoint for igv application
 # Felipe Leza <felipe.leza@databiology.com>
-# Databiology @ 2019
+# Databiology @ 2020
 
 set -euo pipefail
 
@@ -23,10 +23,10 @@ gosu dbe $IGVDIR/igv.sh &
 child=$!
 wait "$child"
 
-if find "/scratch/restults" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
+if find "/scratch/results" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
     echo "Results directory not empty."
 else
-    echo -e "IGV log file\nNo output was generated\nAuto generated file" >> /scratch/results/IGV_log.txt
+    echo -e "IGV log file\nApp finished\nAuto generated file" >> /scratch/results/IGV_log.txt
 fi
 
 echo "IGV stopped"
